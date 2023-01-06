b9845e12124de36277c7e438f2816f7c721f046e
iamteja9977 <thiru21ssc@gmail.com>
2023-01-06 12:47:34 +0530

    arrays and strings and functions done

---
+++ string.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+ string name="thiru";
+ cout<<"the name is "<< name<< endl;
+ cout<<"length of the name is :"<<name.length()<<endl;
+cout<<"substring of the name is "<< name.substr(0,2)<<endl;
+//here end index is excluded
+
+
+}
